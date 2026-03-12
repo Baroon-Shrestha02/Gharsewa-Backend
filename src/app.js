349ceaa1531c842ba98db02aba3975ae3184021c
[firstname lastname] <dhamib610@gmail.com>
2026-03-12 22:58:10 +0545

    modified by app.js

--- src/app.js
+++ src/app.js
@@ -12,14 +12,6 @@ import workerRoutes from "./routes/workerRoutes.js";
 import staffRoutes from "./routes/staffRoutes.js";
 import bookingRoutes from "./routes/bookingRoutes.js";
 import docRoutes from "./routes/kycRoutes.js";
-
-import cookieParser from "cookie-parser";
-import swaggerUi from "swagger-ui-express";
-import fileUpload from "express-fileupload";
-import jobRoutes from "./routes/jobRoutes.js";
-import reviewRoutes from "./Routes/reviewRoutes.js";
-import notificationRoutes from "./Routes/notificationRoutes.js";
-import swaggerSpec from "./Config/swagger.js";
 import jobRoutes from "./routes/jobRoutes.js";
 import reviewRoutes from "./Routes/reviewRoutes.js";
 import notificationRoutes from "./Routes/notificationRoutes.js";
