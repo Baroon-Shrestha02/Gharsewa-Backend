dd69350fe78fc6071f71911d660691aac77924d6
Baroon-Shrestha <bar00nshrestha098@gmail.com>
2026-03-25 11:20:38 +0545

    Updated

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import cloudinary from "cloudinary";
-import Database from "./database/Database.js";
+import Database from "./database/database.js";
 import app from "./app.js";
 import createAdminIfNotExists from "./Utils/CreateAdmin.js";
 
